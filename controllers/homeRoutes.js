0ca34aab041f0a0d4d70608299ff1c5886a37378
[j chun] <[justinchun224@gmail.com]>
2023-10-20 15:27:32 -0700

    created user api routes file

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,1 +1,23 @@
 const router = require('express').Router();
+const {} = require('../models');
+// Will add authCheck to routes later
+const authCheck = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+    // Load landing page. Slide 4 of presentation.
+    console.log('Landing Page')
+});
+
+router.get('/homepage', async (req, res) => {
+    // Load home page. Slide 6.
+    console.log('Home Page')
+})
+
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+      res.redirect('/homepage');
+      return;
+    }
+    // Match login handlebars filename
+    res.render('login');
+  });
