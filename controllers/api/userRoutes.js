9803dba53345414a395b66564022de78620efc9b
[j chun] <[justinchun224@gmail.com]>
2023-10-20 15:34:40 -0700

    added post route for creating new user

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -3,15 +3,16 @@ const { User } = require('../../models');
 
 router.post('/', async (req, res) => {
     try {
-      const newUser = await User.create(req.body);
-  
-      req.session.save(() => {
-        req.session.user_id = newUser.id;
-        req.session.logged_in = true;
-        
-        res.status(200).json(newUser);
-      });
+        const newUser = await User.create(req.body);
+
+        req.session.save(() => {
+            req.session.user_id = newUser.id;
+            req.session.logged_in = true;
+
+            res.status(200).json(newUser);
+        });
     } catch (err) {
-      res.status(400).json(err);
+        res.status(400).json(err);
     }
-  });
+});
+
