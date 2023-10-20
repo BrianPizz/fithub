1a0dfac9683136ec8e004418cda938837c630ad3
BrianPizz <bpizzimentijr@outlook.com>
2023-10-20 14:56:16 -0700

    added outfit products table

--- models/OutfitProducts.js
+++ models/OutfitProducts.js
@@ -1,4 +1,32 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Project extends Model {}
+class OutfitProducts extends Model {}
+
+OutfitProducts.init(
+    {
+        outfit_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'outfit',
+                key: 'id'
+            },
+        },
+        product_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'product',
+                key: 'id'
+            }
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'outfitProducts',
+    }
+);
+
+module.exports = OutfitProducts;
