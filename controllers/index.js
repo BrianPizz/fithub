1b2a4541c50016cf953c9d8473fd28a0b936fa11
[j chun] <[justinchun224@gmail.com]>
2023-10-25 15:46:21 -0700

    deleted dashboard routes in controllers

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,8 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes.js');
-// const dashboardRoutes = require('./dashboardRoutes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
-// router.use('/user', dashboardRoutes);
 
 module.exports = router;
